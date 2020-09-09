b0a86f8877fd328b81f7e4a49bcc8c0221d8bc93
Gary Johnson <lambdatronic@gmail.com>
2020-09-09 15:12:12 -0400

    Updated text in "Importing Rasters into the Database" and added detangling links to import-rasters.sh.

--- resources/import-rasters.sh
+++ resources/import-rasters.sh
@@ -1,3 +1,4 @@
+# [[file:../org/GridFire.org::raster2pgsql-import-example-all][raster2pgsql-import-example-all]]
 #!/bin/sh
 
 DATABASE=gridfire
@@ -7,3 +8,4 @@ for LAYER in asp cbd cbh cc ch dem fbfm13 fbfm40 slp
 do
     raster2pgsql -t 100x100 -I -C $LAYER.tif $SCHEMA.$LAYER | psql -U $DATABASE &
 done
+# raster2pgsql-import-example-all ends here
