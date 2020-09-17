66c9aaca438903f06976f94e312d01c5614136a0
Kenneth Cheung <kcheung331@gmail.com>
2020-09-17 08:58:00 -0700

    run-simulation test added

--- src/sql/create_test_db.sql
+++ src/sql/create_test_db.sql
@@ -7,3 +7,26 @@ CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_raster;
 CREATE SCHEMA landfire;
 GRANT ALL PRIVILEGES ON SCHEMA landfire TO gridfire_test;
+
+INSERT INTO public.spatial_ref_sys (srid, auth_name, auth_srid, srtext, proj4text)
+VALUES (900914, 'user-generated', 900914,
+        'PROJCS["USA_Contiguous_Albers_Equal_Area_Conic_USGS_version",' ||
+        'GEOGCS["NAD83",' ||
+        'DATUM["North_American_Datum_1983",' ||
+        'SPHEROID["GRS 1980",6378137,298.2572221010002,' ||
+        'AUTHORITY["EPSG","7019"]],' ||
+        'AUTHORITY["EPSG","6269"]],' ||
+        'PRIMEM["Greenwich",0],' ||
+        'UNIT["degree",0.0174532925199433],' ||
+        'AUTHORITY["EPSG","4269"]],' ||
+        'PROJECTION["Albers_Conic_Equal_Area"],' ||
+        'PARAMETER["standard_parallel_1",29.5],' ||
+        'PARAMETER["standard_parallel_2",45.5],' ||
+        'PARAMETER["latitude_of_center",23],' ||
+        'PARAMETER["longitude_of_center",-96],' ||
+        'PARAMETER["false_easting",0],' ||
+        'PARAMETER["false_northing",0],' ||
+        'UNIT["metre",1,' ||
+        'AUTHORITY["EPSG","9001"]]]',
+        '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0' ||
+        ' +datum=NAD83 +units=m +no_defs');
--- test/gridfire/cli_test.clj
+++ test/gridfire/cli_test.clj
@@ -1,13 +1,17 @@
 (ns gridfire.cli-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [clojure.java.jdbc :as jdbc]
+  (:require [clojure.core.matrix :as m]
+            [clojure.data.csv :as csv]
+            [clojure.edn :as edn]
             [clojure.java.io :as io]
-            [clojure.core.matrix :as m]
-            [gridfire.cli :refer [fetch-landfire-layers get-envelope]])
+            [clojure.java.jdbc :as jdbc]
+            [clojure.test :refer [deftest is testing]]
+            [magellan.core :as mg]
+            [gridfire.cli :as gf])
   (:import (com.opentable.db.postgres.embedded EmbeddedPostgres
                                                DatabasePreparer
                                                PgBinaryResolver)
-           (org.springframework.core.io ClassPathResource)))
+           (org.springframework.core.io ClassPathResource)
+           (java.util Random)))
 
 ;;-----------------------------------------------------------------------------
 ;; Config
@@ -18,8 +22,25 @@
 (def db-spec {:classname   "org.postgresql.Driver"
               :subprotocol "postgresql"
               :subname     "//localhost:5432/gridfire_test"
-              :user        "postgres"                       ;TODO Change to gridfire_test
+              :user        "postgres" ;TODO Change to gridfire_test
               :password    "password"})
+
+(def test-config-base
+  {:db-spec                   db-spec
+   :srid                      "CUSTOM:900914"
+   :cell-size                 98.425     ;; (feet)
+   :ignition-row              10
+   :ignition-col              20
+   :max-runtime               60         ;; (minutes)
+   :temperature               50         ;; (degrees Fahrenheit)
+   :relative-humidity         1          ;; (%)
+   :wind-speed-20ft           10         ;; (miles/hour)
+   :wind-from-direction       0          ;; (degrees clockwise from north)
+   :foliar-moisture           90         ;; (%)
+   :ellipse-adjustment-factor 1.0        ;; (< 1.0 = more circular, > 1.0 = more elliptical)
+   :simulations               10
+   :random-seed               1234567890 ;; long value (optional)
+   })
 
 ;;-----------------------------------------------------------------------------
 ;; Utils
@@ -33,7 +54,7 @@
 ;;-----------------------------------------------------------------------------
 
 (deftest fetch-landfire-layers-test
-  (testing "fetching layers from postgis and geotiff files"
+  (testing "Fetching layers from postgis and geotiff files"
     (let [postgis-config {:db-spec            db-spec
                           :srid               "CUSTOM:900914"
                           :layer-tables       {:aspect             "landfire.asp WHERE rid=1"
@@ -55,8 +76,8 @@
                                                :fuel-model         (in-file-path "fbfm40.tif")
                                                :slope              (in-file-path "slp.tif")}
                           :fetch-layer-method :geotiff}
-          postgis        (fetch-landfire-layers postgis-config)
-          geotiff        (fetch-landfire-layers geotiff-config)]
+          postgis        (gf/fetch-landfire-layers postgis-config)
+          geotiff        (gf/fetch-landfire-layers geotiff-config)]
 
       (is (= (get-in postgis [:aspect :matrix])
              (get-in geotiff [:aspect :matrix])))
@@ -79,9 +100,86 @@
       (is (= (get-in postgis [:slope :matrix])
              (get-in geotiff [:slope :matrix])))
 
-      (let [postgis-envelope (get-envelope postgis-config postgis)
-            geotiff-envelope (get-envelope geotiff-config geotiff)]
+      ;; TODO Debug failing test
+      (let [postgis-envelope (gf/get-envelope postgis-config postgis)
+            geotiff-envelope (gf/get-envelope geotiff-config geotiff)]
+
+        ;; Signature is not exactly the same
+        (println (.getCoordinateReferenceSystem postgis-envelope))
+        (println (.getCoordinateReferenceSystem geotiff-envelope))
+
         (is (= postgis-envelope geotiff-envelope))
 
-        (is (.equals postgis-envelope geotiff-envelope))
-        ))))
+        (is (.equals postgis-envelope geotiff-envelope))))))
+
+(deftest run-simulation-test
+  (testing "Running simulation with different fetch-layer-method"
+    (let [postgis-config  (merge test-config-base
+                                 {:layer-tables       {:aspect             "landfire.asp WHERE rid=1"
+                                                       :canopy-base-height "landfire.cbh WHERE rid=1"
+                                                       :canopy-cover       "landfire.cc WHERE rid=1"
+                                                       :canopy-height      "landfire.ch WHERE rid=1"
+                                                       :crown-bulk-density "landfire.cbd WHERE rid=1"
+                                                       :elevation          "landfire.dem WHERE rid=1"
+                                                       :fuel-model         "landfire.fbfm40 WHERE rid=1"
+                                                       :slope              "landfire.slp WHERE rid=1"}
+                                  :fetch-layer-method :postgis})
+          geotiff-config  (merge test-config-base
+                                 {:layer-files        {:aspect             (in-file-path "asp.tif")
+                                                       :canopy-base-height (in-file-path "cbh.tif")
+                                                       :canopy-cover       (in-file-path "cc.tif")
+                                                       :canopy-height      (in-file-path "ch.tif")
+                                                       :crown-bulk-density (in-file-path "cbd.tif")
+                                                       :elevation          (in-file-path "dem.tif")
+                                                       :fuel-model         (in-file-path "fbfm40.tif")
+                                                       :slope              (in-file-path "slp.tif")}
+                                  :fetch-layer-method :geotiff})
+          simulations     (:simulations test-config-base)
+          rand-generator  (if-let [seed (:random-seed test-config-base)]
+                            (Random. seed)
+                            (Random.))
+          postgis-layers  (gf/fetch-landfire-layers postgis-config)
+          postgis-results (gf/run-simulations
+                            simulations
+                            (reduce (fn [acc [layer info]] (assoc acc layer (:matrix info)))
+                                    {}
+                                    postgis-layers)
+                            (gf/get-envelope postgis-config postgis-layers)
+                            (:cell-size test-config-base)
+                            (gf/draw-samples rand-generator simulations (:ignition-row test-config-base))
+                            (gf/draw-samples rand-generator simulations (:ignition-col test-config-base))
+                            (gf/draw-samples rand-generator simulations (:max-runtime test-config-base))
+                            (gf/draw-samples rand-generator simulations (:temperature test-config-base))
+                            (gf/draw-samples rand-generator simulations (:relative-humidity test-config-base))
+                            (gf/draw-samples rand-generator simulations (:wind-speed-20ft test-config-base))
+                            (gf/draw-samples rand-generator simulations (:wind-from-direction test-config-base))
+                            (gf/draw-samples rand-generator simulations (:foliar-moisture test-config-base))
+                            (gf/draw-samples rand-generator simulations (:ellipse-adjustment-factor test-config-base))
+                            (:outfile-suffix test-config-base)
+                            (:output-geotiffs? test-config-base)
+                            (:output-pngs? test-config-base)
+                            (:output-csvs? test-config-base))
+
+          geotiff-layers  (gf/fetch-landfire-layers geotiff-config)
+          geotiff-results (gf/run-simulations
+                            simulations
+                            (reduce (fn [acc [layer info]] (assoc acc layer (:matrix info)))
+                                    {}
+                                    geotiff-layers)
+                            (gf/get-envelope geotiff-config geotiff-layers)
+                            (:cell-size test-config-base)
+                            (gf/draw-samples rand-generator simulations (:ignition-row test-config-base))
+                            (gf/draw-samples rand-generator simulations (:ignition-col test-config-base))
+                            (gf/draw-samples rand-generator simulations (:max-runtime test-config-base))
+                            (gf/draw-samples rand-generator simulations (:temperature test-config-base))
+                            (gf/draw-samples rand-generator simulations (:relative-humidity test-config-base))
+                            (gf/draw-samples rand-generator simulations (:wind-speed-20ft test-config-base))
+                            (gf/draw-samples rand-generator simulations (:wind-from-direction test-config-base))
+                            (gf/draw-samples rand-generator simulations (:foliar-moisture test-config-base))
+                            (gf/draw-samples rand-generator simulations (:ellipse-adjustment-factor test-config-base))
+                            (:outfile-suffix test-config-base)
+                            (:output-geotiffs? test-config-base)
+                            (:output-pngs? test-config-base)
+                            (:output-csvs? test-config-base))]
+
+      (is (= (set postgis-results) (set geotiff-results))))))
