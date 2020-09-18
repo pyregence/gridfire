aa18c460287b6ee9e8ce6f885a3c413f4e0f0256
Kenneth Cheung <kcheung331@gmail.com>
2020-09-18 12:26:18 -0700

    fix tests

--- test/gridfire/cli_test.clj
+++ test/gridfire/cli_test.clj
@@ -28,18 +28,19 @@
 (def test-config-base
   {:db-spec                   db-spec
    :srid                      "CUSTOM:900914"
-   :cell-size                 98.425          ;; (feet)
-   :ignition-row              [10 90]
-   :ignition-col              [20 80]
-   :max-runtime               60              ;; (minutes)
-   :temperature               '(50 65 80)     ;; (degrees Fahrenheit)
-   :relative-humidity         '(1 10 20)      ;; (%)
-   :wind-speed-20ft           '(10 15 20)     ;; (miles/hour)
-   :wind-from-direction       '(0 90 180 270) ;; (degrees clockwise from north)
-   :foliar-moisture           90              ;; (%)
-   :ellipse-adjustment-factor 1.0             ;; (< 1.0 = more circular, > 1.0 = more elliptical)
-   :simulations               10
-   :random-seed               1234567890      ;; long value (optional)
+   :cell-size                 98.425     ;; (feet)
+   :ignition-row              [10 10]
+   :ignition-col              [20 20]
+   :max-runtime               60         ;; (minutes)
+   :temperature               '(50)      ;; (degrees Fahrenheit)
+   :relative-humidity         '(1)       ;; (%)
+   :wind-speed-20ft           '(10)      ;; (miles/hour)
+   :wind-from-direction       '(0)       ;; (degrees clockwise from north)
+   :foliar-moisture           90         ;; (%)
+   :ellipse-adjustment-factor 1.0        ;; (< 1.0 = more circular, > 1.0 = more elliptical)
+   :simulations               1
+   :random-seed               1234567890 ;; long value (optional)
+   :output-csvs?              true
    })
 
 ;;-----------------------------------------------------------------------------
@@ -180,5 +181,9 @@
                             (:output-geotiffs? test-config-base)
                             (:output-pngs? test-config-base)
                             (:output-csvs? test-config-base))]
+
+      (is (every? some? postgis-results))
+
+      (is (every? some? geotiff-results))
 
       (is (= (set postgis-results) (set geotiff-results))))))
