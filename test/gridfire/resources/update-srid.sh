7c02ba4ccdca775e7a277d72d8f068cffbeeafb3
Kenneth Cheung <kcheung331@gmail.com>
2020-09-18 13:45:06 -0700

    Ignore files that don't exist in update-srid.sh

--- test/gridfire/resources/update-srid.sh
+++ test/gridfire/resources/update-srid.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 for LAYER in asp cbd cbh cc ch dem fbfm13 fbfm40 slp
-do psql -d gridfire_test -c \
-        "SELECT
-UpdateRasterSRID('landfire'::name,'$LAYER'::name,'rast'::name,900914);" &
+do
+    if test -f $LAYER.tif; then
+        psql -d gridfire_test -c \
+             "SELECT UpdateRasterSRID('landfire'::name,'$LAYER'::name,'rast'::name,900914);" &
+    fi
 done
