949444190f997443f9e9af598093bc48e67e0654
Kenneth Cheung <kcheung331@gmail.com>
2020-11-02 11:38:44 -0800

    add cli controls

--- src/gridfire/config.clj
+++ src/gridfire/config.clj
@@ -1,6 +1,8 @@
 (ns gridfire.config
-  (:require [clojure.string :as str]
-            [clojure.pprint :as pprint]))
+  (:require [clojure.pprint :as pprint]
+            [clojure.string :as str]
+            [clojure.tools.cli :refer [parse-opts]]
+            [gridfire.crown-fire :refer [m->ft]]))
 
 (defn file-path [directory file-name]
   (str directory "/" file-name ".tif"))
@@ -14,24 +16,23 @@
     :else                                  (subs s 1 (dec (count s)))))
 
 (defn parse [s]
-  (->> s
-       (#(clojure.string/split % #"\n"))
-       (mapcat #(clojure.string/split % #"="))
-       (map clojure.string/trim)
-       (remove #(re-matches #"&.*" %))
-       (remove #(re-matches #"/" %))
-       (remove #(re-matches #"" %))
-       (apply (partial assoc {}))
+  (->> (str/split s #"\n")
+       (filter #(str/includes? % "="))
+       (mapcat #(str/split % #"="))
+       (map str/trim)
+       (apply hash-map)
        (reduce-kv (fn [m k v]
-                    (assoc m (keyword k) (convert v)))
+                    (assoc m k (convert v)))
                   {})))
 
 (defn build-edn
-  [{:keys [FUELS_AND_TOPOGRAPHY_DIRECTORY ASP_FILENAME CBH_FILENAME CC_FILENAME
+  [{:strs [FUELS_AND_TOPOGRAPHY_DIRECTORY ASP_FILENAME CBH_FILENAME CC_FILENAME
            CH_FILENAME CBD_FILENAME FBFM_FILENAME SLP_FILENAME DEM_FILENAME
            COMPUTATIONAL_DOMAIN_CELLSIZE PHI_FILENAME MAX_RUNTIME
            STOCHASTIC_TMP_FILENAME STOCHASTIC_RH_FILENAME STOCHASTIC_WS_FILENAME
-           STOCHASTIC_WD_FILENAME FOLIAR_MOISTURE_CONTENT FOLIAR_MOISTURE_CONTENT]}]
+           STOCHASTIC_WD_FILENAME FOLIAR_MOISTURE_CONTENT FOLIAR_MOISTURE_CONTENT
+           SEED]}
+   {:keys [verbose?]}]
   (let [dir FUELS_AND_TOPOGRAPHY_DIRECTORY]
     {:fetch-layer-method        :geotiff
      :landfire-layers           {:aspect             (file-path dir ASP_FILENAME)
@@ -42,7 +43,7 @@
                                  :fuel-model         (file-path dir FBFM_FILENAME)
                                  :slope              (file-path dir SLP_FILENAME)
                                  :elevation          (file-path dir DEM_FILENAME)}
-     :cell-size                 COMPUTATIONAL_DOMAIN_CELLSIZE
+     :cell-size                 (m->ft COMPUTATIONAL_DOMAIN_CELLSIZE)
      :fetch-ignition-method     :geotiff
      :ignition-layer            (file-path dir PHI_FILENAME)
      :max-runtime               MAX_RUNTIME
@@ -52,18 +53,36 @@
      :wind-from-direction       (file-path dir STOCHASTIC_WD_FILENAME)
      :foliar-moisture           FOLIAR_MOISTURE_CONTENT
      :ellipse-adjustment-factor 1.0
+     :simulations               10
+     :random-seed               SEED
+     :outfile-suffix            ""
+     :output-landfire-inputs?   false
+     :output-geotiffs?          false
+     :output-pngs?              verbose?
+     :output-csvs?              verbose?}))
 
-     :simulations 10
-     :random-seed 1234567890 ;; long value (optional)
+(defn write-config [config-params]
+  (let [file "gridfire-config.edn"]
+    (println "Config file:" file)
+    (spit file (with-out-str (pprint/pprint config-params)))))
 
-     :outfile-suffix          ""
-     :output-landfire-inputs? true
-     :output-geotiffs?        true
-     :output-pngs?            true
-     :output-csvs?            true}))
+(defn process-options
+  [{:keys [config-file verbose] :as options}]
+  (let [data (parse (slurp config-file))]
+    (build-edn data options)))
+
+(def cli-options
+  [["-c" "--config-file FILE" "Path to an data file containing a map of simulation configs"]
+   ["-v" "--verbose?" "Flag for controlling outputs"]])
 
 (defn -main
-  [data-file]
-  (let [data          (parse (slurp data-file))
-        output-config (build-edn data)]
-    (spit "generated-config.edn" (with-out-str (pprint/pprint output-config)))))
+  [& args]
+  (println (str "Converting configuration file to one that Gridfire accepts."))
+  (let [{:keys [options arguments summary errors]} (parse-opts args cli-options)]
+    (if (or (seq errors) (empty? options))
+      (do
+        (when (seq errors)
+          (run! println errors)
+          (newline))
+        (println (str "Usage:\n" summary)))
+      (write-config (process-options options)))))
