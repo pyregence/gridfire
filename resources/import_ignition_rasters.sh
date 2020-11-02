c4421f31d90887ab77e49d601b1239959dc71465
Kenneth Cheung <kcheung331@gmail.com>
2020-11-02 15:06:41 -0800

    remove tiling from import ignition raster

--- resources/import_ignition_rasters.sh
+++ resources/import_ignition_rasters.sh
@@ -3,5 +3,5 @@ SCHEMA=$2
 
 for LAYER in ign
 do
-    raster2pgsql -t 100x100 -I -C $LAYER.tif $SCHEMA.$LAYER | psql -h localhost -U $DATABASE
+    raster2pgsql -I -C $LAYER.tif $SCHEMA.$LAYER | psql -h localhost -U $DATABASE
 done
