00e355448e4ee038e72050ae06f5706a764b0046
Kenneth Cheung <kcheung331@gmail.com>
2020-11-16 20:09:30 -0800

    remove :burning from spec

--- src/gridfire/spec/ignition.clj
+++ src/gridfire/spec/ignition.clj
@@ -4,10 +4,9 @@
 
 (s/def ::unburned float?)
 (s/def ::burned float?)
-(s/def ::burning float?)
 
 (s/def ::values
-  (s/keys :req-un [::unburned ::burned ::burning]))
+  (s/keys :req-un [::unburned ::burned]))
 
 (s/def ::ignition-layer
   (s/keys :req-un [(or ::common/sql ::common/path) ::values]))
@@ -15,5 +14,4 @@
 ;;TODO Remove before pushing
 (def test-config {:ignition-layer {:path        "test/gridfire/resources/ign.tif"
                                    :burn-values {:unburned 1.0
-                                                 :burned   -1.0
-                                                 :burning  0.0}}})
+                                                 :burned   -1.0}}})
