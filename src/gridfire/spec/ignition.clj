488cec622557725732a0c07ccaa14e9f7b42a7d3
Kenneth Cheung <kcheung331@gmail.com>
2020-11-17 09:27:48 -0800

    remove todo

--- src/gridfire/spec/ignition.clj
+++ src/gridfire/spec/ignition.clj
@@ -2,16 +2,11 @@
   (:require [clojure.spec.alpha :as s]
             [gridfire.spec.common :as common]))
 
-(s/def ::unburned float?)
 (s/def ::burned float?)
+(s/def ::unburned float?)
 
-(s/def ::values
+(s/def ::burn-values
   (s/keys :req-un [::unburned ::burned]))
 
 (s/def ::ignition-layer
-  (s/keys :req-un [(or ::common/sql ::common/path) ::values]))
-
-;;TODO Remove before pushing
-(def test-config {:ignition-layer {:path        "test/gridfire/resources/ign.tif"
-                                   :burn-values {:unburned 1.0
-                                                 :burned   -1.0}}})
+  (s/keys :req-un [(or ::common/sql ::common/path) ::burn-values]))
