6ccde8fec82e50588e91bd55a0ec1d3ebf8d97f6
Kenneth Cheung <kcheung331@gmail.com>
2020-11-18 13:14:43 -0800

    remove unused import

--- test/gridfire/spec/ignition_test.clj
+++ test/gridfire/spec/ignition_test.clj
@@ -1,19 +1,18 @@
 (ns gridfire.spec.ignition-test
-  (:require [gridfire.spec.ignition :as sut]
-            [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as gen]
-            [clojure.test :refer [deftest is testing]]))
+  (:require [clojure.spec.alpha :as s]
+            [clojure.test :refer [deftest is]]
+            [gridfire.spec.ignition :as ignition]))
 
 (deftest path-test
   (let [path "/some/path/to.tif"]
-    (is (s/valid? ::sut/ignition-layer path))))
+    (is (s/valid? ::ignition/ignition-layer path))))
 
 (deftest sql-test
   (let [sql "landfire.ign WHERE rid=1"]
-    (is (s/valid? ::sut/ignition-layer sql))))
+    (is (s/valid? ::ignition/ignition-layer sql))))
 
 (deftest sql-test
-  (let [sql {:path "/some/path/to.tif"
-             :burn-values {:burned 1.0
+  (let [sql {:path        "/some/path/to.tif"
+             :burn-values {:burned   1.0
                            :unburned -1.0}}]
     (is (s/valid? ::sut/ignition-layer sql))))
