2382e529c5bb5ec6afa41e7d33a036b718598ed9
Kenneth Cheung <kcheung331@gmail.com>
2020-12-09 19:05:10 -0800

    add specific heat of gas

--- src/gridfire/spec/spotting.clj
+++ src/gridfire/spec/spotting.clj
@@ -3,8 +3,10 @@
             [clojure.spec.common :as common]))
 
 (s/def ::ambient-gas-density float?)
+(s/def ::specific-heat-gas float?)
 (s/def ::firebrand-count int?)
 
 (s/def ::spotting
   (one-or-more-keys [::ambient-gas-density
+                     ::specific-heat-gas
                      ::firebrand-count]))
