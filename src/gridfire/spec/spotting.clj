a6936d1eba2414cb20119aa8e7862575ba585d77
Kenneth Cheung <kcheung331@gmail.com>
2020-12-10 16:26:56 -0800

    fix namespace require

--- src/gridfire/spec/spotting.clj
+++ src/gridfire/spec/spotting.clj
@@ -1,12 +1,12 @@
 (ns gridfire.spec.spotting
   (:require [clojure.spec.alpha :as s]
-            [clojure.spec.common :as common]))
+            [gridfire.spec.common :as common]))
 
 (s/def ::ambient-gas-density float?)
 (s/def ::specific-heat-gas float?)
 (s/def ::num-firebrands int?)
 
 (s/def ::spotting
-  (one-or-more-keys [::ambient-gas-density
+  (common/one-or-more-keys [::ambient-gas-density
                      ::specific-heat-gas
                      ::num-firebrands]))
