658e1b13416fbbb4038c9364708520f898cd23f0
Gary Johnson <lambdatronic@gmail.com>
2021-01-26 14:59:33 -0500

    Added read-matrix-as-binary and switched to use Int32 for matrix indices.

--- src/gridfire/binary_output.clj
+++ src/gridfire/binary_output.clj
@@ -1,5 +1,5 @@
 (ns gridfire.binary-output
-  (:import java.io.DataOutputStream)
+  (:import (java.io DataInputStream DataOutputStream))
   (:require [clojure.java.io :as io]
             [clojure.core.matrix :as m]))
 
@@ -16,14 +16,40 @@
                            :value (map (fn [col] (m/mget matrix true-row col)) cols)})))
          (apply merge-with concat))))
 
+(defn indices-to-matrix [indices]
+  (let [rows    (indices :y)
+        cols    (indices :x)
+        values  (indices :value)
+        max-row (apply max rows)
+        max-col (apply max cols)
+        matrix  (make-array Float/TYPE max-row max-col)]
+    (dotimes [i (count values)]
+      (let [true-row (- max-row (rows i))
+            true-col (dec (cols i))]
+        (aset matrix true-row true-col (values i))))
+    (m/matrix matrix)))
+
 (defn write-matrix-as-binary [matrix file-name]
   (let [num-burned-cells (m/non-zero-count matrix)
         burned-data      (non-zero-data matrix)]
     (with-open [out (DataOutputStream. (io/output-stream file-name))]
       (.writeInt out (int num-burned-cells))                          ; Int32
-      (doseq [x (burned-data :x)] (.writeShort out (int x)))          ; Int16
-      (doseq [y (burned-data :y)] (.writeShort out (int y)))          ; Int16
+      (doseq [x (burned-data :x)] (.writeInt out (int x)))            ; Int32
+      (doseq [y (burned-data :y)] (.writeInt out (int y)))            ; Int32
       (doseq [v (burned-data :value)] (.writeFloat out (float v)))))) ; Float32
+
+(defn read-matrix-as-binary [file-name]
+  (with-open [in (DataInputStream. (io/input-stream file-name))]
+    (let [num-burned-cells (.readInt in)]                                ; Int32
+      (indices-to-matrix
+       {:x     (vec (repeatedly num-burned-cells #(.readInt in)))        ; Int32
+        :y     (vec (repeatedly num-burned-cells #(.readInt in)))        ; Int32
+        :value (vec (repeatedly num-burned-cells #(.readFloat in)))})))) ; Float32
+
+(defn write-two-int-file [file-name]
+  (with-open [out (DataOutputStream. (io/output-stream file-name))]
+    (.writeInt out 1)   ; Int32
+    (.writeInt out 2))) ; Int32
 
 ;; toa_test.bin was created with:
 ;; (write-matrix-as-binary [[0 1 2] [3 0 4] [5 6 0]] "toa_test.bin")
