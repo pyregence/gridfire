7267dbe10f693be42f170f810193624e11fb4759
Kenneth Cheung <kcheung331@gmail.com>
2021-02-01 14:29:41 -0800

    update spec with new fetching config

--- test/gridfire/config_validation_test.clj
+++ test/gridfire/config_validation_test.clj
@@ -21,11 +21,54 @@
 ;; Validator tests
 ;;-----------------------------------------------------------------------------
 
+(deftest valid-landfire-geotiff-bacwards-compatable-test
+  (let [config {:aspect             (in-file-path "asp.tif")
+                :canopy-base-height (in-file-path "cbh.tif")
+                :canopy-cover       (in-file-path "cc.tif")
+                :canopy-height      (in-file-path "ch.tif")
+                :crown-bulk-density (in-file-path "cbd.tif")
+                :elevation          (in-file-path "dem.tif")
+                :fuel-model         (in-file-path "fbfm40.tif")
+                :slope              (in-file-path "slp.tif")}]
+    (is (s/valid? ::validation/landfire-layers config))))
+
+(deftest valid-landfire-geotiff-test
+  (let [config {:aspect             {:type   :geotiff
+                                     :source (in-file-path "asp.tif")}
+                :canopy-base-height {:type   :geotiff
+                                     :source (in-file-path "cbh.tif")}
+                :canopy-cover       {:type   :geotiff
+                                     :source (in-file-path "cc.tif")}
+                :canopy-height      {:type   :geotiff
+                                     :source (in-file-path "ch.tif")}
+                :crown-bulk-density {:type   :geotiff
+                                     :source (in-file-path "cbd.tif")}
+                :elevation          {:type   :geotiff
+                                     :source (in-file-path "dem.tif")}
+                :fuel-model         {:type   :geotiff
+                                     :source (in-file-path "fbfm40.tif")}
+                :slope              {:type   :geotiff
+                                     :source (in-file-path "slp.tif")}}]
+    (is (s/valid? ::validation/landfire-layers config))))
+
+(deftest valid-weather-geotiff-test
+  (let [config {:type      :geotiff
+                :source    (in-file-path "weather-test/tmpf_to_sample.tif")
+                :cell-size 10.0}]
+    (is (s/valid? ::validation/weather config))))
+
+(deftest valid-weather-postgis-test
+  (let [config {:type      :postgis
+                :source    "weather.ws WHERE rid=1"
+                :cell-size 10.0}]
+    (is (s/valid? ::validation/weather config))))
+
 (deftest weather-cell-size-test
   (let [high-res (m->ft 30)
         low-res  (* high-res 10)
         temp     (s/conform ::validation/weather
-                            {:path      (in-file-path "weather-test/tmpf_to_sample.tif")
+                            {:type      :geotiff
+                             :source    (in-file-path "weather-test/tmpf_to_sample.tif")
                              :cell-size low-res})]
     (testing "Valid cell-size for a weather raster"
       (let [config {:cell-size   high-res
@@ -44,25 +87,10 @@
   (testing "Invalid cell-size for a weather raster"
     (let [cell-size (m->ft 30)
           temp      (s/conform ::validation/weather
-                               {:path      (in-file-path "weather-test/tmpf_to_sample.tif")
+                               {:type      :geotiff
+                                :source    (in-file-path "weather-test/tmpf_to_sample.tif")
                                 :cell-size (+ cell-size (/ cell-size 2))})
           config    {:cell-size   cell-size
                      :temperature temp}]
 
       (is (false? (validation/valid-weather-cell-sizes? config))))))
-
-(deftest weather-fetch-method-test
-  (testing "Weather raster input (string) must have accompanying fetch method keyword"
-    (let [temp   (s/conform ::validation/weather
-                            (in-file-path "weather-test/tmpf_to_sample.tif"))
-          config {:temperature temp}]
-
-      (is (false? (validation/valid-weather-fetch-methods? config)))))
-
-  (testing "Weather raster input (map) must have accompanying fetch method keyword"
-    (let [temp   (s/conform ::validation/weather
-                            {:path      (in-file-path "weather-test/tmpf_to_sample.tif")
-                             :cell-size 80.0})
-          config {:temperature temp}]
-
-      (is (false? (validation/valid-weather-fetch-methods? config))))))
