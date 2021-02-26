b292d513cbeeff74482dc24dbfd52df399108a46
Kenneth Cheung <kcheung331@gmail.com>
2021-02-26 09:07:23 -0800

    add to-imperial multimethod for converting to imperial units

--- src/gridfire/conversion.clj
+++ src/gridfire/conversion.clj
@@ -1,4 +1,5 @@
-(ns gridfire.conversion)
+(ns gridfire.conversion
+  (:require [clojure.core.matrix :as m]))
 
 (defn F->K
   "Convert farenheight to kelvin."
@@ -6,11 +7,24 @@
   (->> (+ degrees 459.67)
        (* (/ 5.0 9.0))))
 
+(defn K->F
+  "Convert kelvin to farenheight."
+  [degrees]
+  (->> (- degrees 273.15)
+       (* (/ 9.0 5.0))
+       (+ 32.0)))
+
 (defn F->C
   "Convert farenheight to celcius."
   [degrees]
   (->> (- degrees 32.0)
        (* (/ 5.0 9.0))))
+
+(defn C->F
+  "Convert celsius to farenheight."
+  [degrees]
+  (->> (* degrees (/ 9.0 5.0))
+       (+ 32.0)))
 
 (defn deg->rad
   "Convert degrees to radians."
@@ -49,3 +63,70 @@
   "Convert seconds to minutes."
   [s]
   (/ s 60))
+
+(def conversion
+  {:elevation          m->ft
+   :slope              deg->rad
+   :canopy-height      m->ft
+   :canopy-base-height m->ft
+   :crown-bulk-density #(* % 0.0624) ; kg/m^3 -> lb/ft^3
+   :wind-speed-20ft    #(* % 2.237)  ; m/s -> mph
+   :temperature        {:metric   C->F
+                        :absolute K->F}})
+
+(defmulti to-imperial (fn [_ _ layer-name] layer-name))
+
+(defmethod to-imperial :elevation
+  [layer {:keys [units multiplier]} layer-name]
+  (if-let [xforms (seq (remove nil? [(when (= units :metric) (conversion layer-name))
+                                     (when-not (contains? #{1 1.0 nil} multiplier) #(* % multiplier))]))]
+    (update layer :matrix (fn [matrix] (m/emap (apply comp xforms) matrix)))
+    layer))
+
+(defmethod to-imperial :slope
+  [layer {:keys [multiplier]} layer-name]
+  (if-let [xforms (seq (remove nil? [(conversion layer-name)
+                                     (when-not (contains? #{1 1.0 nil} multiplier) #(* % multiplier))]))]
+    (update layer :matrix (fn [matrix] (m/emap (apply comp xforms) matrix)))
+    layer))
+
+(defmethod to-imperial :canopy-height
+  [layer {:keys [units multiplier]} layer-name]
+  (if-let [xforms (seq (remove nil? [(when (= units :metric) (conversion layer-name))
+                                     (when-not (contains? #{1 1.0 nil} multiplier) #(* % multiplier))]))]
+    (update layer :matrix (fn [matrix] (m/emap (apply comp xforms) matrix)))
+    layer))
+
+(defmethod to-imperial :canopy-base-height
+  [layer {:keys [units multiplier]} layer-name]
+  (if-let [xforms (seq (remove nil? [(when (= units :metric) (conversion layer-name))
+                                     (when-not (contains? #{1 1.0 nil} multiplier) #(* % multiplier))]))]
+    (update layer :matrix (fn [matrix] (m/emap (apply comp xforms) matrix)))
+    layer))
+
+(defmethod to-imperial :crown-bulk-density
+  [layer {:keys [units multiplier]} layer-name]
+  (if-let [xforms (seq (remove nil? [(when (= units :metric) (conversion layer-name))
+                                     (when-not (contains? #{1 1.0 nil} multiplier) #(* % multiplier))]))]
+    (update layer :matrix (fn [matrix] (m/emap (apply comp xforms) matrix)))
+    layer))
+
+(defmethod to-imperial :wind-speed-20ft
+  [layer {:keys [units multiplier]} layer-name]
+  (if-let [xforms (seq (remove nil? [(when (= units :metric) (conversion layer-name))
+                                     (when-not (contains? #{1 1.0 nil} multiplier) #(* % multiplier))]))]
+    (update layer :matrix (fn [matrix] (m/emap (apply comp xforms) matrix)))
+    layer))
+
+(defmethod to-imperial :temperature
+  [layer {:keys [units multiplier]} layer-name]
+  (if-let [xforms (seq (remove nil? [(cond
+                                       (= units :metric)   (get-in conversion [layer-name units])
+                                       (= units :absolute) (get-in conversion [layer-name units]))
+                                     (when-not (contains? #{1 1.0 nil} multiplier) #(* % multiplier))]))]
+    (update layer :matrix (fn [matrix] (m/emap (apply comp xforms) matrix)))
+    layer))
+
+(defmethod to-imperial :default
+  [layer _ _]
+  layer)
