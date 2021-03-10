ae54fbbeae1e98afe8f1d98df07b051dade5e333
Kenneth Cheung <kcheung331@gmail.com>
2021-03-10 10:53:44 -0800

    add server

---
+++ src/gridfire/server.clj
@@ -0,0 +1,35 @@
+(ns gridfire.server
+  (:require [gridfire.simple-sockets :as sockets]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.core.async :refer [timeout]]
+            [clojure.data.json :as json]))
+
+(def cli-options
+  [["-p" "--port PORT" "Port number"
+    :default 31337
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]])
+
+;; TODO This handler, after receiving response from provisioning server needs to:
+;; unzip tar file in incoming and put into data folder
+;; run gridfire.config/write-config to convert elmfire.data -> gridfire.edn
+;; run gridfire simulation with gridfire.edn
+(defn handler [msg]
+  (let [{:keys [response-host response-port] :as response} (json/read-str msg :key-fn keyword)]
+    (timeout 500)
+    (println "Message:" response)
+    (sockets/send-to-server! response-host (Integer/parseInt response-port) (json/write-str {:message "success"}))))
+
+(defn start-server! [& args]
+  (let [{:keys [options summary errors]} (parse-opts args cli-options)]
+    (if (or (seq errors) (empty? options))
+      (do
+        (when (seq errors)
+          (run! println errors)
+          (newline))
+        (println (str "Usage:\n" summary)))
+      (let [port (:port options)]
+        (println (format "Running Server on port %s" port))
+        (sockets/start-server! (:port options) handler)))))
+
+(def -main start-server!)
---
+++ src/gridfire/simple_sockets.clj
@@ -0,0 +1,70 @@
+(ns gridfire.simple-sockets
+  (:import (java.io BufferedReader)
+           (java.net Socket ServerSocket))
+  (:require [clojure.java.io :as io]
+            [clojure.string  :as s]
+            [triangulum.logging :refer [log log-str]]))
+
+;;=================================
+;; Client Socket
+;;=================================
+
+(defn send-to-server! [host port message]
+  (with-open [socket (Socket. host port)]
+    (doto (io/writer socket)
+      (.write (-> message
+                  (s/trim-newline)
+                  (str "\n")))
+      (.flush))
+    (.shutdownOutput socket)))
+
+#_(send-to-server! "wx.pyregence.org"
+                   31337
+                   "clj-socket-test2,2021-03-01 12:00 PDT,-117.5,33.8,24000,24000,24000,24000,no,yes,localhost")
+
+;;=================================
+;; Server Socket
+;;=================================
+
+(defonce ^:private global-server-thread (atom nil))
+(defonce ^:private global-server-socket (atom nil))
+
+(defn- read-socket! [^Socket socket]
+  (.readLine ^BufferedReader (io/reader socket)))
+
+(defn- accept-connections! [^ServerSocket server-socket handler]
+  (while @global-server-thread
+    (try
+      (let [socket (.accept server-socket)]
+        (try
+          (->> (read-socket! socket)
+               (handler))
+          (catch Exception e
+            (log-str "Server error: " e))
+          (finally (.close socket))))
+      (catch Exception _))))
+
+(defn stop-server! []
+  (if @global-server-thread
+    (do
+      (reset! global-server-thread nil)
+      (when @global-server-socket
+        (.close ^ServerSocket @global-server-socket)
+        (reset! global-server-socket nil))
+      (log "Server stopped."))
+    (log "Server is not running.")))
+
+(defn start-server! [port handler]
+  (if @global-server-thread
+    (log "Server is already running.")
+    (reset! global-server-thread
+            (future
+              (try
+                (with-open [server-socket (ServerSocket. port)]
+                  (reset! global-server-socket server-socket)
+                  (accept-connections! server-socket handler))
+                (catch Exception e
+                  (log-str "Error creating server socket: " e)
+                  (stop-server!)))))))
+
+#_(start-server! 31337 (fn [msg] :do-something))
