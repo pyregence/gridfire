ddbef253550b8fdc781b6bb8f5c48702da1538c7
Kenneth Cheung <kcheung331@gmail.com>
2021-04-02 12:05:16 -0700

    add truncate false to log

--- src/gridfire/cli.clj
+++ src/gridfire/cli.clj
@@ -408,7 +408,7 @@
                                (r/remove nil?)
                                (reducer-fn))]
     (Thread/sleep 1000)
-    (log-str (tufte/format-grouped-pstats @stats-accumulator {:format-pstats-opts {:columns [:n-calls :min :max :mean :mad :clock :total]}}))
+    (log (tufte/format-grouped-pstats @stats-accumulator {:format-pstats-opts {:columns [:n-calls :min :max :mean :mad :clock :total]}}) :truncate? false)
     {:burn-count-matrix burn-count-matrix
      :summary-stats     summary-stats}))
 
