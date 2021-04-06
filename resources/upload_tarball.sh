6648ed20e2e7e71ac3019f3070f4c38ccd215dd6
Kenneth Cheung <kcheung331@gmail.com>
2021-04-05 22:14:50 -0700

    fix upload_tarball.sh missing var declarations

--- resources/upload_tarball.sh
+++ resources/upload_tarball.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+MODEL=gridfire
+FIRENAME=`echo $(pwd) | rev | cut -d'/' -f2 | rev | cut -d_ -f1`
+START_DATE=`echo $(pwd) | rev | cut -d'/' -f2 | rev | cut -d_ -f2`
+START_TIME=`echo $(pwd) | rev | cut -d'/' -f2 | rev | cut -d_ -f3`
+
 cd geoserver
 scp  $MODEL-$FIRENAME-${START_DATE}_${START_TIME}.tar gridfire@data.pyregence.org:/incoming/
 mv *.tar ..
