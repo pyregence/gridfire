1667e97b104846042438f3d3f061f2ef2fbe552c
Kenneth Cheung <kcheung331@gmail.com>
2021-04-05 22:10:25 -0700

    uncomment upload_tarball.sh

--- resources/upload_tarball.sh
+++ resources/upload_tarball.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd geoserver
-# scp  $MODEL-$FIRENAME-${START_DATE}_${START_TIME}.tar gridfire@data.pyregence.org:/incoming/
+scp  $MODEL-$FIRENAME-${START_DATE}_${START_TIME}.tar gridfire@data.pyregence.org:/incoming/
 mv *.tar ..
-# ssh gridfire@data.pyregence.org \
-#     tar -xf /incoming/$MODEL-$FIRENAME-${START_DATE}_${START_TIME}.tar \
-#     -C /var/www/html/fire_spread_forecast
+ssh gridfire@data.pyregence.org \
+    tar -xf /incoming/$MODEL-$FIRENAME-${START_DATE}_${START_TIME}.tar \
+    -C /var/www/html/fire_spread_forecast
 
 echo "Uploaded tarball to GeoServer"
 
