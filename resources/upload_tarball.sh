f082664942ccbb55a611c2bf3b9fa6e8a9f8d207
Kenneth Cheung <kcheung331@gmail.com>
2021-05-25 15:59:06 -0700

    update upload_tarball script to put into /incoming/match_drop dir

--- resources/upload_tarball.sh
+++ resources/upload_tarball.sh
@@ -9,7 +9,7 @@ cd geoserver
 scp  $MODEL-$FIRENAME-${START_DATE}_${START_TIME}.tar gridfire@data.pyregence.org:/incoming/
 mv *.tar ..
 ssh gridfire@data.pyregence.org \
-    tar -xf /incoming/$MODEL-$FIRENAME-${START_DATE}_${START_TIME}.tar \
+    tar -xf /incoming/match_drop/$MODEL-$FIRENAME-${START_DATE}_${START_TIME}.tar \
     -C /var/www/html/fire_spread_forecast
 
 echo "Uploaded tarball to GeoServer"
