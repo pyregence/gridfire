a4d629f1d1a2a6accb68a51948a5c37dec492679
Kenneth Cheung <kcheung331@gmail.com>
2021-06-29 14:24:11 -0700

    Add delay to wait for scp to complete before adding job to queue

--- src/gridfire/active_fire_watcher.clj
+++ src/gridfire/active_fire_watcher.clj
@@ -1,9 +1,13 @@
 (ns gridfire.active-fire-watcher
-  (:require [nextjournal.beholder :as beholder]
-            [triangulum.logging   :refer [log-str]]
-            [clojure.core.async   :refer [>!! go >!]])
-
+  (:require [clojure.core.async :refer [<! >!! go-loop timeout]]
+            [nextjournal.beholder :as beholder]
+            [triangulum.logging :refer [log-str]])
   (:import java.util.TimeZone))
+
+;;-----------------------------------------------------------------------------
+;; Utils
+;;-----------------------------------------------------------------------------
+(defonce download-in-progress (atom {}))
 
 (def file-name-regex #"[^/]*(?=[.][a-zA-Z]+$)")
 
@@ -25,19 +29,38 @@
   (let [file-name     (re-find file-name-regex path)
         fire-name     (re-find fire-name-regex file-name)
         ignition-time (re-find ignition-time-regex file-name)]
-    [fire-name (convert-date-string ignition-time "yyyyMMdd_HHmmss" "yyyy-MM-dd HH:mm zzz")]))
+    [fire-name (convert-date-string ignition-time
+                                    "yyyyMMdd_HHmmss"
+                                    "yyyy-MM-dd HH:mm zzz")]))
+
+(defn build-job [path]
+  (let [[fire-name ignition-time] (parse-tar path)]
+    {:fire-name     fire-name
+     :ignition-time ignition-time
+     :type          :active-fire}))
+
+(defn count-down [job-queue tar]
+  (go-loop [seconds (get @download-in-progress tar)]
+    (if (> @seconds 0)
+      (do (<! (timeout 1000))
+          (swap! seconds dec)
+          (recur (get @download-in-progress tar)))
+      (do (log-str "SCP Complete:" tar)
+          (>!! job-queue (build-job tar))))))
+
+;;-----------------------------------------------------------------------------
+;; Utils
+;;-----------------------------------------------------------------------------
 
 (defn- handler [job-queue]
   (fn [{:keys [type path]}]
-    (log-str "Active Fire input deck detected: " type ":" (.toString path))
-    (go
-     (when (= type :create)
-       (let [[fire-name ignition-time] (parse-tar (.toString path))
-             job                       {:fire-name     fire-name
-                                        :ignition-time ignition-time
-                                        :type          :active-fire}]
-         (Thread/sleep 30000)
-         (>! job-queue job))))))
+    (let [path-str  (.toString path)]
+      (case type
+        :create (do (log-str "Active Fire input deck detected:" path-str)
+                    (swap! download-in-progress assoc path-str (atom 5))
+                    (count-down job-queue path-str))
+        :modify (swap! download-in-progress assoc path-str (atom 5)) ;reset counter
+        nil))))
 
 (defn start! [{:keys [active-fire-dir]} job-queue]
   (when active-fire-dir
