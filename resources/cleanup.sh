071455077552ce682c60e426b6ec6291c64c0beb
Kenneth Cheung <kcheung331@gmail.com>
2021-09-17 10:58:15 -0700

    PR Fixes: resource scripts and docs

--- resources/cleanup.sh
+++ resources/cleanup.sh
@@ -3,6 +3,6 @@
 echo "Compressing binary files and removing originals"
 tar cf - *.bin | pigz > binary_outputs.tar.gz && rm -f -r *.bin already_burned_float.tif phi.tif elmfire_post.data diag*.csv geoserver geoserver_new scratch &
 
-echo "Done compresing and cleaning up"
+echo "Done compressing and cleaning up"
 
 exit 0
--- resources/import_weather_rasters.sh
+++ resources/import_weather_rasters.sh
@@ -6,11 +6,9 @@ SCHEMA=$2
 SRID=$3
 TILING=$4
 
-echo $4
-
 for LAYER in tmpf wd ws rh
 do
-    if [ -z "$4" ]
+    if [ -z "$TILING" ]
     then
         raster2pgsql -I -C -t auto -s $SRID ${LAYER}_to_sample.tif $SCHEMA.$LAYER | psql -h localhost -U $USERNAME
     else
