6b03d04de5e7f19d556f4ce9863e9c78d4c2914b
Gary Johnson <lambdatronic@gmail.com>
2021-10-29 18:37:48 -0400

    Formatting cleanup in gridfire.spec.server

--- src/gridfire/spec/server.clj
+++ src/gridfire/spec/server.clj
@@ -1,17 +1,18 @@
 (ns gridfire.spec.server
   (:require [clojure.spec.alpha    :as spec]
-            [gridfire.utils.server :refer [hostname? port? time? fire-name?]]))
+            [gridfire.utils.server :refer [hostname? port? fire-name? time?]]))
 
-(spec/def ::response-host                    hostname?)
-(spec/def ::response-port                    port?)
-(spec/def ::fire-name                        fire-name?)
-(spec/def ::ignition-time                    time?)
-(spec/def ::type                             #{:active-fire}) ; TODO: Add more types as needed
-(spec/def ::gridfire-server-request          (spec/keys :req-un [::fire-name
-                                                                 ::ignition-time]
-                                                        :opt-un [::response-host
-                                                                 ::response-port
-                                                                 ::type]))
+(spec/def ::response-host hostname?)
+(spec/def ::response-port port?)
+(spec/def ::fire-name     fire-name?)
+(spec/def ::ignition-time time?)
+(spec/def ::type          #{:active-fire}) ; TODO: Add more types as needed
 
-(spec/def ::gridfire-server-response-minimal (spec/keys :req-un [::response-host
-                                                                 ::response-port]))
+(spec/def ::gridfire-server-request
+  (spec/keys
+   :req-un [::fire-name ::ignition-time]
+   :opt-un [::response-host ::response-port ::type]))
+
+(spec/def ::gridfire-server-response-minimal
+  (spec/keys
+   :req-un [::response-host ::response-port]))
