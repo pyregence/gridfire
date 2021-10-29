62cb060b8c264e5a02506bdf31d8a8e493acf606
Gary Johnson <lambdatronic@gmail.com>
2021-10-29 18:23:02 -0400

    Added ::number-sample, ::integer-sample, ::float-sample, ::layer-coords, and ::file-path specs to gridfire.spec.common

--- src/gridfire/spec/common.clj
+++ src/gridfire/spec/common.clj
@@ -1,40 +1,68 @@
 (ns gridfire.spec.common
   (:require [clojure.spec.alpha :as s]))
 
-;;-----------------------------------------------------------------------------
-;; Regex
-;;-----------------------------------------------------------------------------
+;;=============================================================================
+;; Numeric Samples
+;;=============================================================================
+
+(s/def ::number-sample
+  (s/or :scalar number?
+        :list   (s/coll-of number? :kind list?)
+        :vector (s/coll-of number? :kind vector? :count 2)))
+
+(s/def ::integer-sample
+  (s/or :scalar integer?
+        :list   (s/coll-of integer? :kind list?)
+        :vector (s/coll-of integer? :kind vector? :count 2)))
+
+(s/def ::float-sample
+  (s/or :scalar float?
+        :list   (s/coll-of float? :kind list?)
+        :vector (s/coll-of float? :kind vector? :count 2)))
+
+;;=============================================================================
+;; Layer Coords
+;;=============================================================================
 
 (def postgis-sql-regex #"[a-z0-9]+(\.[a-z0-9]+)? WHERE rid=[0-9]+")
 
 (def path-to-geotiff-regex #"[a-z_\-\s0-9\.\/]+(\/[a-z_\-\s0-9\.]+)*\.tif")
 
-;;-----------------------------------------------------------------------------
+(s/def ::sql (s/and string? #(re-matches postgis-sql-regex %)))
+
+(s/def ::geotiff (s/and string? #(re-matches path-to-geotiff-regex %)))
+
+(s/def ::source (s/or :sql     ::sql
+                      :geotiff ::geotiff))
+
+(s/def ::type #{:geotiff :postgis})
+
+(s/def ::cell-size number?)
+
+(s/def ::unit #{:imperial :metric})
+
+(s/def ::multiplier number?)
+
+(s/def ::postgis-or-geotiff
+  (s/keys :req-un [::source ::type]
+          :opt-un [::cell-size ::unit ::multiplier]))
+
+(s/def ::layer-coords (s/or :sql ::sql
+                            :map ::postgis-or-geotiff))
+
+;;=============================================================================
+;; File Access
+;;=============================================================================
+
+(def file-path-regex #"^(((\.\.){1}/)*|(/){1})?(([\w-]*)/)*([\w-]+)$")
+
+(s/def ::file-path (s/and string? #(re-matches file-path-regex %)))
+
+;;=============================================================================
 ;; Macros
-;;-----------------------------------------------------------------------------
+;;=============================================================================
 
 (defmacro one-or-more-keys [ks]
   (let [keyset (set (map (comp keyword name) ks))]
     `(s/and (s/keys :opt-un ~ks)
             #(some ~keyset (keys %)))))
-
-;;-----------------------------------------------------------------------------
-;; Spec
-;;-----------------------------------------------------------------------------
-
-(s/def ::sql (s/and string? #(re-matches postgis-sql-regex %)))
-
-(s/def ::path (s/and string? #(re-matches path-to-geotiff-regex %)))
-
-(s/def ::source (s/or :file-path ::path
-                      :sql       ::sql))
-
-(s/def ::type #(contains? #{:geotiff :postgis} %))
-
-(s/def ::unit #(contains? #{:imperial :metric} %))
-
-(s/def ::multiplier (s/or :int int? :float float?))
-
-(s/def ::postgis-or-geotiff
-  (s/keys :req-un [::type ::source]
-          :opt-un [::cell-size ::unit ::multiplier]))
