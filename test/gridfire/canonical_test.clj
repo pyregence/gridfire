b666559a0f20046be17f9d6a7d30304ed6e5ce6e
RJ Sheperd <rjsheperd@gmail.com>
2022-01-11 10:34:07 -0800

    GRID-254 Canonical crowning outputs

--- test/gridfire/canonical_test.clj
+++ test/gridfire/canonical_test.clj
@@ -3,6 +3,7 @@
             [clojure.java.io      :as io]
             [clojure.data.csv     :as csv]
             [clojure.spec.alpha   :as s]
+            [clojure.string       :as str]
             [clojure.test         :refer [deftest is run-tests]]
             [gridfire.core        :as gf]
             [gridfire.spec.config :as spec])
@@ -11,17 +12,26 @@
 
 ;;; Constants
 
-(def ^:private canonical-dir     "test/gridfire/resources/canonical_test/")
-(def ^:private base-config       (edn/read-string (slurp (str canonical-dir "base-config.edn"))))
-(def ^:private summary-stats-csv "summary_stats.csv")
-(def ^:private scenarios         {:fuel-model         [:grass-fbfm40 :timber-litter-fbfm40]
-                                  :canopy-cover       [:zero-raster :raster-100]
-                                  :slope              [:zero-raster :slp-10 :slp-20 :slp-30]
-                                  :wind-speed-20ft    [0 10 20 40]
-                                  :fuel-moisture      (range 0.0 0.25 0.05)
-                                  :foliar-moisture    [0 0.5 1.0]
-                                  :canopy-base-height [:zero-raster :raster-2 :raster-10 :raster-20 :raster-40]
-                                  :crown-bulk-density [:zero-raster :cbd-02 :cbd-035 :cbd-05]})
+(def ^:private canonical-dir      "test/gridfire/resources/canonical_test/")
+(def ^:private base-config        (edn/read-string (slurp (str canonical-dir "base-config.edn"))))
+(def ^:private summary-stats-csv  "summary_stats.csv")
+(def ^:private surface-scenarios  {:fuel-model         [:grass-fbfm40 :timber-litter-fbfm40 :grass-extreme-fbfm40 :shrub-fbfm40 :blowdown-fbfm40]
+                                   :canopy-cover       [:zero-raster]
+                                   :slope              [:zero-raster :slp-10 :slp-20 :slp-30]
+                                   :wind-speed-20ft    [0 10 20 40]
+                                   :fuel-moisture      (range 0.0 0.25 0.05)
+                                   :foliar-moisture    [0 0.5 1.0]
+                                   :canopy-base-height [:zero-raster]
+                                   :crown-bulk-density [:zero-raster]})
+
+(def ^:private crowning-scenarios {:fuel-model         [:grass-extreme-fbfm40 :shrub-fbfm40 :blowdown-fbfm40]
+                                   :canopy-cover       [:raster-100]
+                                   :slope              [:zero-raster]
+                                   :wind-speed-20ft    [:ws_20ft-zero_24_zero]
+                                   :fuel-moisture      [0.1]
+                                   :foliar-moisture    [0.1]
+                                   :canopy-base-height [:raster-2 :raster-10 :raster-20 :raster-40]
+                                   :crown-bulk-density [:cbd-0005 :cbd-001 :cbd-005 :cbd-01 :cbd-02 :cbd-035 :cbd-05]})
 
 ;;; Helpers
 
@@ -71,9 +81,15 @@
   {:dead (zipmap [:1hr :10hr :100hr]  (repeat fuel-moisture))
    :live (zipmap [:woody :herbaceous] (repeat fuel-moisture))})
 
+(defn- dir-name [in]
+  (cond
+    (float? in)   (-> in (str) (str/replace #"\." "_"))
+    (keyword? in) (name in)
+    :else         (str in)))
 
 (defn- output-directory [{:keys [base-dir
                                  datetime
+                                 scenario-type
                                  fuel-model
                                  canopy-cover
                                  slope
@@ -85,26 +101,27 @@
   (str (when base-dir base-dir)
        "outputs"
        "/" datetime
-       "/fuel-model_" (name fuel-model)
-       "/slope_" (name slope)
-       "/wind-speed-20ft_" wind-speed-20ft
+       "/" (dir-name scenario-type)
+       "/fuel-model_" (dir-name fuel-model)
+       "/slope_" (dir-name slope)
+       "/wind-speed-20ft_" (dir-name wind-speed-20ft)
        "/fuel-moisture_" (int (* 100 fuel-moisture))
        "/canopy-cover_" (name canopy-cover)
        "/foliar-moisture_" (int (* 100 foliar-moisture))
-       "/canopy-base-height_" (name canopy-base-height)
-       "/crown-bulk-density_" (name crown-bulk-density)
+       "/canopy-base-height_" (dir-name canopy-base-height)
+       "/crown-bulk-density_" (dir-name crown-bulk-density)
        "/"))
 
 (defn- result->row [{:keys [params summary-stats]}]
   (let [stats (first summary-stats)]
-    [(-> params :fuel-model name)
-     (-> params :slope name)
-     (:wind-speed-20ft params)
+    [(-> params :fuel-model dir-name)
+     (-> params :slope dir-name)
+     (-> params :wind-speed-20ft dir-name)
      (:fuel-moisture params)
-     (-> params :canopy-cover name)
+     (-> params :canopy-cover dir-name)
      (:foliar-moisture params)
-     (-> params :canopy-base-height name)
-     (-> params :crown-bulk-density name)
+     (-> params :canopy-base-height dir-name)
+     (-> params :crown-bulk-density dir-name)
      (:simulation stats)
      (:ignition-row stats)
      (:ignition-col stats)
@@ -165,14 +182,14 @@
                                   :elevation    (->dem-tif slope)}
                :fuel-moisture    (->fuel-moisture-map fuel-moisture)
                :foliar-moisture  foliar-moisture
-               :wind-speed-20ft  wind-speed-20ft
+               :wind-speed-20ft  (if (keyword? wind-speed-20ft) (->tif wind-speed-20ft) wind-speed-20ft)
                :output-directory (output-directory (assoc params :datetime datetime))}
               (when-not (some #(= :zero-raster %) [canopy-cover canopy-base-height crown-bulk-density])
                 {:landfire-layers {:canopy-base-height (->tif canopy-base-height)
                                    :canopy-height      (->ch canopy-base-height)
                                    :crown-bulk-density (->tif crown-bulk-density)}})))
 
-(defn- gen-scenarios []
+(defn- gen-scenarios [scenario-type scenarios]
   (deep-flatten
     (let [datetime (now)]
       (for [fuel-model         (:fuel-model scenarios)
@@ -183,7 +200,8 @@
             wind-speed-20ft    (:wind-speed-20ft scenarios)
             canopy-base-height (:canopy-base-height scenarios)
             crown-bulk-density (:crown-bulk-density scenarios)]
-        (gen-scenario {:fuel-model         fuel-model
+        (gen-scenario {:scenario-type      scenario-type
+                       :fuel-model         fuel-model
                        :canopy-base-height canopy-base-height
                        :canopy-cover       canopy-cover
                        :crown-bulk-density crown-bulk-density
@@ -203,13 +221,14 @@
     (is (= (slurp control-stats)
            (slurp new-stats)))))
 
-(deftest all-scenarios
-  (let [test-file (str "test-"(now)".csv")]
-    (results->csv test-file (pmap run-sim! (gen-scenarios)))))
+(deftest ^:surface test-surface-scenarios
+  (let [test-file (str "test-surface-"(now)".csv")]
+    (results->csv test-file (pmap run-sim! (gen-scenarios :surface surface-scenarios)))))
 
+(deftest ^:crowning test-crowning-scenarios
+  (let [test-file (str "test-crowning-"(now)".csv")]
+    (results->csv test-file (pmap run-sim! (gen-scenarios :crowning crowning-scenarios)))))
 
 (comment
-
   (run-tests 'gridfire.canonical-test)
-
   )
