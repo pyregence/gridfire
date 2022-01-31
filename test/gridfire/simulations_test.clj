148566bb360d3e6481390b63cb0367267f783f09
Kenneth Cheung <kcheung331@gmail.com>
2022-01-31 13:28:55 -0500

    fix fetch_ignition_test and simulations_test

--- test/gridfire/simulations_test.clj
+++ test/gridfire/simulations_test.clj
@@ -1,28 +1,30 @@
 (ns gridfire.simulations-test
   (:require [gridfire.simulations :as simulations]
-            [clojure.test :refer [deftest is testing]])
+            [clojure.test :refer [deftest is testing]]
+            [tech.v3.tensor :as t])
   (:import java.util.Random))
 
 (defn- identical-matrix [band width height value]
-  (into-array
-   (repeat band
-           (into-array
-            (repeat height
-                    (into-array
-                     (repeat width value)))))))
+  (t/->tensor
+   (into-array
+    (repeat band
+            (into-array
+             (repeat height
+                     (into-array
+                      (repeat width value))))))))
 
 (deftest get-layer-fn-test
   (testing "no perturbations"
     (testing "scalar"
-      (let [inputs       {:perturbations nil
-                          :temperature-samples  [10]}
+      (let [inputs       {:perturbations       nil
+                          :temperature-samples [10]}
             get-layer-fn (#'simulations/get-value-fn inputs (Random. 1234) :temperature 0)]
 
         (is (= 10 (get-layer-fn 0 0 0)))))
 
     (testing "matrix"
-      (let [inputs       {:perturbations nil
-                          :temperature-matrix   (identical-matrix 72 10 10 1.0)}
+      (let [inputs       {:perturbations      nil
+                          :temperature-matrix (identical-matrix 72 10 10 1.0)}
             get-layer-fn (#'simulations/get-value-fn inputs (Random. 1234) :temperature 0)]
 
         (is (= 1.0 (get-layer-fn 0 0 0))))))
@@ -30,17 +32,17 @@
   (testing "with perturbations"
     (testing "scalar"
       (testing "spatial type pixel"
-        (let [inputs       {:perturbations {:temperature {:spatial-type :pixel
-                                                          :range        [-1.0 1.0]}}
-                            :temperature-samples  [10]}
+        (let [inputs       {:perturbations       {:temperature {:spatial-type :pixel
+                                                                :range        [-1.0 1.0]}}
+                            :temperature-samples [10]}
               get-layer-fn (#'simulations/get-value-fn inputs (Random. 1234) :temperature 0)]
 
           (is (<= 9.0 (get-layer-fn 0 0 0) 11.0))))
 
       (testing "spatial type global"
-        (let [inputs       {:perturbations {:temperature {:spatial-type :global
+        (let [inputs {:perturbations       {:temperature {:spatial-type :global
                                                           :range        [-1.0 1.0]}}
-                            :temperature-samples  [10]}
+                      :temperature-samples [10]}
 
               get-layer-fn (#'simulations/get-value-fn inputs (Random. 1234) :temperature 0)]
 
@@ -48,17 +50,17 @@
 
     (testing "matrix"
       (testing "spatial type pixel"
-        (let [inputs       {:perturbations {:temperature {:spatial-type :pixel
-                                                          :range        [-1.0 1.0]}}
-                            :temperature-matrix   (identical-matrix 72 100 100 1.0)}
+        (let [inputs       {:perturbations      {:temperature {:spatial-type :pixel
+                                                               :range        [-1.0 1.0]}}
+                            :temperature-matrix (identical-matrix 72 100 100 1.0)}
               get-layer-fn (#'simulations/get-value-fn inputs (Random. 1234) :temperature 0)]
 
           (is (<= 0.0 (get-layer-fn 0 0 0) 2.0))))
 
       (testing "spatial type global"
-        (let [inputs       {:perturbations {:temperature {:spatial-type :global
-                                                          :range        [-1.0 1.0]}}
-                            :temperature-matrix   (identical-matrix 72 100 100 1.0)}
+        (let [inputs       {:perturbations      {:temperature {:spatial-type :global
+                                                               :range        [-1.0 1.0]}}
+                            :temperature-matrix (identical-matrix 72 100 100 1.0)}
               get-layer-fn (#'simulations/get-value-fn inputs (Random. 1234) :temperature 0)]
 
           (is (<= 0.0 (get-layer-fn 0 0 0) 2.0)))))))
