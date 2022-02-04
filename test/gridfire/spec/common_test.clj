d72a34006e9f5cd0813b0ee4af2c14354a1f17af
Kenneth Cheung <kcheung331@gmail.com>
2022-02-04 10:34:38 -0500

    fix file-path-regex

--- test/gridfire/spec/common_test.clj
+++ test/gridfire/spec/common_test.clj
@@ -44,3 +44,21 @@
                   :hi [2.0 3.0]}]
       (is (not (s/valid? ::common/number-or-range-map config))
           "should not have overlapping ranges"))))
+
+(deftest ^:unit file-path-test
+  (is (s/valid? ::common/file-path "/absolute/file/path"))
+  (is (s/valid? ::common/file-path "./relative/file/path"))
+  (is (s/valid? ::common/file-path "../relative/file/path"))
+  (is (s/valid? ::common/file-path "../../relative/file/path"))
+  (is (not (s/valid? ::common/file-path "./relative/file/path/")))
+  (is (not (s/valid? ::common/file-path "../relative/file/path/")))
+  (is (not (s/valid? ::common/file-path "./relative/file/path/"))))
+
+(deftest ^:unit directory-path-test
+  (is (s/valid? ::common/directory-path "/absolute/directory/path"))
+  (is (s/valid? ::common/directory-path "/absolute/directory/path/"))
+  (is (s/valid? ::common/directory-path "../relative/directory/path/"))
+  (is (s/valid? ::common/directory-path "../../relative/directory/path/"))
+  (is (s/valid? ::common/directory-path "../relative/directory/path"))
+  (is (s/valid? ::common/directory-path "./relative/directory/path/"))
+  (is (s/valid? ::common/directory-path "./relative/directory/path")))
