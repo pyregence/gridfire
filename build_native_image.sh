aa9ba12b954fa66a592d54ca52a52fe9973f004e
Gary Johnson <lambdatronic@gmail.com>
2022-02-11 12:17:45 -0500

    Even more

--- build_native_image.sh
+++ build_native_image.sh
@@ -29,15 +29,19 @@ clojure -M -e "(compile 'gridfire.gen-raster)"
     --trace-object-instantiation=java.util.TimerThread \
     --trace-object-instantiation=java.io.RandomAccessFile \
     --trace-object-instantiation=org.geotools.util.WeakCollectionCleaner \
+    --trace-object-instantiation=it.geosolutions.imageio.utilities.WeakCollectionCleaner \
+    --initialize-at-run-time=it.geosolutions.imageio.utilities.SoftValueHashMap \
     --trace-object-instantiation=java.util.zip.Inflater \
     --initialize-at-run-time=org.apache.logging.log4j.core.async.AsyncLoggerContext \
     --initialize-at-run-time=org.apache.logging.log4j.core.config.yaml.YamlConfiguration \
     --initialize-at-run-time=org.apache.logging.log4j.core.pattern.JAnsiTextRenderer \
     --initialize-at-run-time=com.sun.media.imageioimpl.plugins.pnm.PNMImageWriter \
     --trace-object-instantiation=com.sun.jmx.mbeanserver.JmxMBeanServer \
+    --initialize-at-run-time=com.sun.jmx.mbeanserver.JmxMBeanServer \
+    --initialize-at-run-time=javax.management.MBeanServerFactory \
+    --initialize-at-run-time=org.geotools.util.WeakValueHashMap \
     --trace-object-instantiation=java.util.Random \
     gridfire.gen_raster
-
     # "-J-Dclojure.compiler.direct-linking=true" \
     # --no-server
     # --report-unsupported-elements-at-runtime \
@@ -45,4 +49,3 @@ clojure -M -e "(compile 'gridfire.gen-raster)"
     # -H:ConfigurationFileDirectories=. \
     # org.geotools.util.SoftValueHashMap
     # --initialize-at-run-time=org.geotools.referencing.factory.DeferredAuthorityFactory.TIMER \
-    # org.geotools.util.WeakValueHashMap
