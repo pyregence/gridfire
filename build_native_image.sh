0db0740cf0936adb0c0c1dc2eb4036d73603a93a
Gary Johnson <lambdatronic@gmail.com>
2022-02-11 09:20:39 -0500

    Add build_native_image.sh

---
+++ build_native_image.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [ -z "$GRAALVM_HOME" ]; then
+    echo "Please set \$GRAALVM_HOME"
+    exit 1
+fi
+
+if [ ! -e "$GRAALVM_HOME/bin/native-image" ]; then
+    echo "Installing the native-image command..."
+    "$GRAALVM_HOME/bin/gu" install native-image
+fi
+
+echo "Compiling Clojure to Java bytecode..."
+clojure -M -e "(compile 'gridfire.cli)"
+
+"$GRAALVM_HOME/bin/native-image" \
+    -cp "$(clojure -Spath)":classes \
+    -H:Name=gridfire-native-image \
+    -H:+ReportExceptionStackTraces \
+    --initialize-at-build-time \
+    --verbose \
+    --no-fallback \
+    --no-server \
+    gridfire.cli
+
+    # "-J-Xmx100G" \
+    # --report-unsupported-elements-at-runtime \
+    # "-J-Dclojure.compiler.direct-linking=true" \
