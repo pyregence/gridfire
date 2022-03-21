6cd34216a7f93e617bba3fa167277f3e4b23e36e
Kenneth Cheung <kcheung331@gmail.com>
2022-03-21 18:59:33 -0400

    optimize merging of spot-ignitions

--- src/gridfire/fire_spread_optimal.clj
+++ src/gridfire/fire_spread_optimal.clj
@@ -343,6 +343,20 @@
             {}
             ignited-cells)))
 
+(defn- merge-spot-ignitions [a b]
+  (loop [to-process (seq b)
+         acc        a]
+    (if (seq to-process)
+      (let [[cell spot-info] (first to-process)]
+        (if-let [existing-entry (get acc cell)]
+          (let [[_ cur-p] existing-entry
+                [_ new-p] spot-info]
+            (recur (rest to-process) (assoc acc cell (if (> ^double cur-p ^double new-p )
+                                                       existing-entry
+                                                       spot-info))))
+          (recur (rest to-process) (assoc acc cell spot-info))))
+      acc)))
+
 (defn- compute-spot-burn-vectors!
   [{:keys [num-rows num-cols cell-size get-elevation fuel-model-matrix] :as inputs}
    {:keys
@@ -353,9 +367,9 @@
         new-spot-ignitions       (compute-new-spot-ignitions inputs matrices ignited-cells)
         [spot-ignite-later
          spot-ignite-now]        (identify-spot-ignition-events new-clock
-                                                                (merge-with (fn [x y] (if (> ^double (x 1) ^double (y 1)) x y))
-                                                                            spot-ignitions
-                                                                            new-spot-ignitions))
+                                                                (merge-spot-ignitions
+                                                                 spot-ignitions
+                                                                 new-spot-ignitions))
         ignited?                 (fn [[k v]]
                                    (let [[i j] k
                                          [_ p] v]
