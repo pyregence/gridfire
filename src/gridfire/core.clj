5f0b6cf75782dd0b6dd03dcf77a225fae6857e65
Kenneth Cheung <kcheung331@gmail.com>
2022-07-19 10:52:44 -0400

    fix paren

--- src/gridfire/core.clj
+++ src/gridfire/core.clj
@@ -60,7 +60,7 @@
       (inputs/add-random-ignition-sites)
       (inputs/add-aggregate-matrices)
       (inputs/add-burn-period-params)
-      (inputs/add-ignition-start-times))
+      (inputs/add-ignition-start-times)))
 
 (defn load-config!
   [config-file-path]
