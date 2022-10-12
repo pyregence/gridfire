a8da0db2704018e1bef21b1d2d04b4cc609a2df2
Valentin Waeselynck <val.vvalval@gmail.com>
2022-10-12 18:24:14 +0200

    Revert "Revert "Added gridfire.utils.files.tar.""
    This reverts commit 427de3ecd501c26def8a9f30674dfe5cdc346efc.

---
+++ src/gridfire/utils/files/tar.clj
@@ -0,0 +1,32 @@
+(ns gridfire.utils.files.tar
+  (:require [clojure.java.io :as io])
+  (:import (org.apache.commons.compress.compressors.gzip GzipCompressorInputStream)
+           (org.apache.commons.compress.archivers.tar TarArchiveInputStream TarArchiveEntry)
+           (java.io ByteArrayOutputStream EOFException)))
+
+(defn targz-input-stream
+  "Convenience function for obtaining a TarArchiveInputStream from a tar.gz input."
+  ^TarArchiveInputStream
+  [archive-input]
+  (-> archive-input
+      (io/input-stream)
+      (GzipCompressorInputStream.)
+      (TarArchiveInputStream.)))
+
+(defn tar-archive-entries
+  "Returns a lazy sequence of [entry-name entry-bytes-array] pairs for each entry
+  in the given TAR archive."
+  [^TarArchiveInputStream tais]
+  (lazy-seq
+   (when-some [^TarArchiveEntry my-entry (.getNextTarEntry tais)]
+     (try
+       (let [entry-name  (.getName my-entry)
+             entry-bytes (with-open [os (ByteArrayOutputStream.)]
+                           (io/copy tais os)
+                           (.toByteArray os))]
+         (cons
+          [entry-name entry-bytes]
+          (tar-archive-entries tais)))
+       ;; WARNING sometimes thrown for no apparent reason... (Val, 26 Sep 2022)
+       (catch EOFException _eofex
+         ())))))
