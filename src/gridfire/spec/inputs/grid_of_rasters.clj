c1fd7ee2b3506f62bf7bb2d56aa5ea7f799d403e
Valentin Waeselynck <val.vvalval@gmail.com>
2022-10-27 14:23:17 +0200

    [doc] added comments and removed dead code about :grid_of_rasters.

--- src/gridfire/spec/inputs/grid_of_rasters.clj
+++ src/gridfire/spec/inputs/grid_of_rasters.clj
@@ -4,6 +4,19 @@
 (s/def ::type #{:grid_of_rasters})
 
 (s/def ::rasters_grid
+  ;; NOTE the blocks in the rasters grid must be arranged in an order
+  ;; which is consistent with the layout of the sub-tensors.
+  ;; Typically, if inside each block the pixels are arranged
+  ;; in a [y decreasing, x increasing] layout,
+  ;; then the blocks must be arranged in the same way.
+  ;; An equivalent way of saying this is that
+  ;; the blocks are arranged in a way which is GIS-agnostic,
+  ;; and natural for describing a matrix by blocks:
+  ;; for example, traversing the stitched matrix in the [i j] -> [i+1 j] direction
+  ;; will traverse the blocks in the [grid-i grid-j] -> [grid-i+1 grid-j] direction.
+  ;; WARNING: this organizing principle might feel at odds with your GIS intuition
+  ;; of organizing blocks in a [x increasing, y increasing] layout:
+  ;; if so, don't blame :rasters_grid - blame the sub-tensors.
   (s/coll-of (s/coll-of :gridfire.spec.common/raw-layer-coords-map)))
 
 (s/def ::raw-layer-coords-map (s/keys :req-un [::type ::rasters_grid]))
