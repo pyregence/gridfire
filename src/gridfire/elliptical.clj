cf5bedd7bbc805a83d8fe0ca77f1ca27e01290f9
Valentin Waeselynck <val.vvalval@gmail.com>
2022-11-30 18:44:15 +0100

    [ref] minor style and doc fixes.

--- src/gridfire/elliptical.clj
+++ src/gridfire/elliptical.clj
@@ -1,7 +1,6 @@
 (ns gridfire.elliptical
   "Computations based on the Elliptical Wavelets model.")
 
-;; FIXME tests from gridfire-scripts
 (defn fireline-normal-spread-rate-scalar
   "Computes the ratio of fireline-normal spread rate to maximal (heading) spread rate,
   in our idealized elliptical-wavelet model
@@ -13,11 +12,12 @@
   from the unit vector orthogonal to the fireline (pointing towards the unburned area)
   and the unit vector in the direction of maximum spread (in flat terrain, the wind direction)
   -equivalently, cos-ang is the dot-product between those unit vectors-
-  e.g cos-ang = 1.0 for heading fire, cos-ang = -1.0 for backing fire,
+  e.g. cos-ang = 1.0 for heading fire, cos-ang = -1.0 for backing fire,
   and cos-ang = 0 for flanking fire;
 
-  returns the fireline-normal spread rate (in ft/min),
-  i.e the speed at which this segment of the fireline advances
+  returns a number by which to downscale max-spread-rate to get
+  the fireline-normal spread rate,
+  i.e. the speed at which this segment of the fire line advances
   orthogonally to itself."
   ^double
   [^double eccentricity
