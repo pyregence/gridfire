2d99fd481da90e8845350efdbcc3eb7ffcb61e8a
Valentin Waeselynck <val.vvalval@gmail.com>
2023-05-09 15:23:22 +0200

    (catch Exception ...) -> (catch Throwable ...) to catch more issues like OutOfMemoryError.

--- src/gridfire/utils/server.clj
+++ src/gridfire/utils/server.clj
@@ -12,7 +12,7 @@
 (defmacro nil-on-error
   [& body]
   (let [_ (gensym)]
-    `(try ~@body (catch Exception ~_ nil))))
+    `(try ~@body (catch Throwable ~_ nil))))
 
 (defn non-empty-string?
   [x]
